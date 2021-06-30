f7cbcbe193122879ea82f81463d40bcd554ae747
angelgamedev <angel.rodriguez.gamedev@gmail.com>
2021-06-30 09:14:39 -0400

    Progress. Viewmodes and switching.

--- TopDownMetroidvaniaVR/Assets/Scripts/CameraController.cs
+++ TopDownMetroidvaniaVR/Assets/Scripts/CameraController.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public enum EViewMode
+    {
+        Isometric = 0,
+        Perspective = 1,
+        TopDown = 2
+    }
+
     public static CameraController Main = null;
 
     public Transform Target;
+
+    [Space(10)]
+
+    public EViewMode ViewMode;
 
     [Space(10)]
 
@@ -15,24 +26,37 @@ public class CameraController : MonoBehaviour
 
     [Space(5)]
 
-    public float MinHeight;
-    public float MaxHeight;
-    public float HeightAdjustSpeed;
+    [Header("Isometric")]
+
+    public float CameraArmLengthIso;
+    public float OrthoMin;
+    public float OrthoMax;
+    public float ZoomSpeedScrollIso;
+    public float ZoomSpeedButtonIso;
+    public float MinHeightIso;
+    public float MaxHeightIso;
+    public float HeightAdjustSpeedIso;
 
     [Space(5)]
 
-    public float OrthoMin;
-    public float OrthoMax;
-    public float ZoomSpeedScroll;
-    public float ZoomSpeedButton;
+    [Header("Perspective")]
+
+    public float CameraArmLengthPerspective;
+    public float ZoomSpeedScrollPerspective;
+    public float ZoomSpeedButtonPerspective;
+    public float ZoomArmlengthMin;
+    public float ZoomArmLengthMax;
+    public float MinHeightPerspective;
+    public float MaxHeightPerspective;
+    public float HeightAdjustSpeedPerspective;
 
     private Camera RefCamera;
 
-    private float CameraArmLength;
     private Vector3 ActiveOffset;
     private float ActiveAngle;
     private float FixedTargetHeight;
-    private float HeightOffset;
+    private float HeightOffsetIso;
+    private float HeightOffsetPerspective;
 
     private void Awake()
     {
@@ -50,50 +74,130 @@ public class CameraController : MonoBehaviour
     {
         RefCamera = GetComponent<Camera>();
 
-        HeightOffset = Mathf.Sqrt(transform.position.y);
+        HeightOffsetIso = Mathf.Sqrt(transform.position.y);
+        HeightOffsetPerspective = 10;
+
         ActiveAngle = transform.eulerAngles.y + 180f;
-
-        CameraArmLength = Vector3.Distance
-        (
-            new Vector3
-            (
-                transform.position.x,
-                0f,
-                transform.position.z
-            ),
-            new Vector3
-            (
-                Target.position.x,
-                0f,
-                Target.position.z
-            )
-        );
-
-        Debug.Log(CameraArmLength);
 
         FixedTargetHeight = Target.position.y; // Saves this to use forever.
     }
 
     private void Update()
     {
+        // Switch view mode
+        if (Input.GetButtonDown("ToggleView"))
+        {
+            if (ViewMode == EViewMode.Isometric)
+            {
+                ViewMode = EViewMode.Perspective;
+            }
+            else if (ViewMode == EViewMode.Perspective)
+            {
+                ViewMode = EViewMode.TopDown;
+            }
+            else if (ViewMode == EViewMode.TopDown)
+            {
+                ViewMode = EViewMode.Isometric;
+            }
+        }
+
+        if (ViewMode == EViewMode.Isometric)
+        {
+            UpdateIsometric();
+        }
+        else if (ViewMode == EViewMode.Perspective)
+        {
+            UpdatePerspective();
+        }
+        else if (ViewMode == EViewMode.TopDown)
+        {
+            UpdateTopDown();
+        }
+    }
+
+    private void UpdateIsometric()
+    {
+        if (!RefCamera.orthographic)
+        {
+            RefCamera.orthographic = true;
+        }
+
         // Horizontal rotation.
         ActiveAngle += Input.GetAxis("LookHorizontal") * TurnSpeed * Time.deltaTime;
 
-        ActiveOffset = Quaternion.AngleAxis(ActiveAngle, Vector3.up) * (Vector3.forward * CameraArmLength);
+        ActiveOffset = Quaternion.AngleAxis(ActiveAngle, Vector3.up) * (Vector3.forward * CameraArmLengthIso);
+
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, 0f);
 
         // Vertical tilt.
-        HeightOffset += Input.GetAxis("LookTilt") * HeightAdjustSpeed * Time.deltaTime;
-        HeightOffset = Mathf.Clamp(HeightOffset, MinHeight, MaxHeight);
+        HeightOffsetIso += Input.GetAxis("LookTilt") * HeightAdjustSpeedIso * Time.deltaTime;
+        HeightOffsetIso = Mathf.Clamp(HeightOffsetIso, MinHeightIso, MaxHeightIso);
 
-        RefCamera.orthographicSize += Input.GetAxis("Scrollwheel") * ZoomSpeedScroll * Time.deltaTime;
-        RefCamera.orthographicSize += Input.GetAxis("Zoom") * ZoomSpeedButton * Time.deltaTime;
+        RefCamera.orthographicSize += Input.GetAxis("Scrollwheel") * ZoomSpeedScrollIso * Time.deltaTime;
+        RefCamera.orthographicSize += Input.GetAxis("Zoom") * ZoomSpeedButtonIso * Time.deltaTime;
         RefCamera.orthographicSize = Mathf.Clamp(RefCamera.orthographicSize, OrthoMin, OrthoMax);
+    }
+
+    private void UpdatePerspective()
+    {
+        if (RefCamera.orthographic)
+        {
+            RefCamera.orthographic = false;
+        }
+
+        // Horizontal rotation.
+        ActiveAngle += Input.GetAxis("LookHorizontal") * TurnSpeed * Time.deltaTime;
+
+        ActiveOffset = Quaternion.AngleAxis(ActiveAngle, Vector3.up) * (Vector3.forward * CameraArmLengthPerspective);
+
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, 0f);
+
+        // Vertical tilt.
+        HeightOffsetPerspective += Input.GetAxis("LookTilt") * HeightAdjustSpeedPerspective * Time.deltaTime;
+        HeightOffsetPerspective = Mathf.Clamp(HeightOffsetPerspective, MinHeightPerspective, MaxHeightPerspective);
+
+        // Zoom with CameraArmLengthPerspective
+        CameraArmLengthPerspective += Input.GetAxis("Scrollwheel") * ZoomSpeedScrollPerspective * Time.deltaTime;
+        CameraArmLengthPerspective += Input.GetAxis("Zoom") * ZoomSpeedScrollPerspective * Time.deltaTime;
+        CameraArmLengthPerspective = Mathf.Clamp(CameraArmLengthPerspective, ZoomArmlengthMin, ZoomArmLengthMax);
+    }
+
+    private void UpdateTopDown()
+    {
+        if (RefCamera.orthographic)
+        {
+            RefCamera.orthographic = false;
+        }
+
+        // Horizontal rotation.
+        ActiveAngle += Input.GetAxis("LookHorizontal") * TurnSpeed * Time.deltaTime;
+
+        // Zoom with CameraArmLengthPerspective
+        CameraArmLengthPerspective += Input.GetAxis("Scrollwheel") * ZoomSpeedScrollPerspective * Time.deltaTime;
+        CameraArmLengthPerspective += Input.GetAxis("Zoom") * ZoomSpeedScrollPerspective * Time.deltaTime;
+        CameraArmLengthPerspective = Mathf.Clamp(CameraArmLengthPerspective, ZoomArmlengthMin, ZoomArmLengthMax);
     }
 
     private void LateUpdate()
     {
+        if (ViewMode == EViewMode.Isometric)
+        {
+            LateUpdateIsometric();
+        }
+        else if (ViewMode == EViewMode.Perspective)
+        {
+            LateUpdatePerspective();
+        }
+        else if (ViewMode == EViewMode.TopDown)
+        {
+            LateUpdateTopDown();
+        }
+    }
+
+    private void LateUpdateIsometric()
+    {
         Vector3 adjustedTargetPos = Target.position;
-        adjustedTargetPos.y = FixedTargetHeight + (HeightOffset * HeightOffset);
+        adjustedTargetPos.y = FixedTargetHeight + (HeightOffsetIso * HeightOffsetIso);
 
         adjustedTargetPos += ActiveOffset;
 
@@ -101,5 +205,32 @@ public class CameraController : MonoBehaviour
         transform.position = Vector3.Lerp(transform.position, adjustedTargetPos, FollowSpeed * Time.deltaTime);
 
         transform.LookAt(Target);
+    }
+
+    private void LateUpdatePerspective()
+    {
+        Vector3 adjustedTargetPos = Target.position;
+        adjustedTargetPos.y = FixedTargetHeight + HeightOffsetPerspective;
+
+        adjustedTargetPos += ActiveOffset;
+
+        // Smoothly move to active target pos.
+        transform.position = Vector3.Lerp(transform.position, adjustedTargetPos, FollowSpeed * Time.deltaTime);
+
+        transform.LookAt(Target);
+    }
+
+    private void LateUpdateTopDown()
+    {
+        Vector3 adjustedTargetPos = Target.position;
+        adjustedTargetPos.y = FixedTargetHeight + HeightOffsetPerspective;
+
+        adjustedTargetPos += ActiveOffset;
+
+        // Smoothly move to active target pos.
+        transform.position = Vector3.Lerp(transform.position, adjustedTargetPos, FollowSpeed * Time.deltaTime);
+
+        transform.LookAt(Target);
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, ActiveAngle);
     }
 }
